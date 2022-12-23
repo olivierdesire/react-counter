36b857fb416b8e6a158766f7c529db494fd499bd
Olivier Désiré <olivierdesire@MacBook-Pro-de-Olivier.local>
2022-12-23 13:29:58 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import logo from "./assets/img/calculette.png";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [button1, setButton1] = useState(false);
+  const [button2, setButton2] = useState(true);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <img src={logo} alt="logo" />
+        <p>React Counter</p>
       </header>
+      <main>
+        <div className="list-counter">
+          <button
+            onClick={() => {
+              setCounter(counter - 1);
+              counter - 1 === 0 ? setButton1(false) : setButton1(true);
+              counter - 1 === 10 ? setButton2(false) : setButton2(true);
+            }}
+            style={{ visibility: button1 ? "visible" : "hidden" }}
+          >
+            −
+          </button>
+          <div className="counter">{counter}</div>
+          <button
+            onClick={() => {
+              setCounter(counter + 1);
+              // console.log(counter);
+              counter + 1 === 10 ? setButton2(false) : setButton2(true);
+              counter + 1 === 0 ? setButton1(false) : setButton1(true);
+            }}
+            style={{ visibility: button2 ? "visible" : "hidden" }}
+          >
+            +
+          </button>
+        </div>
+        <button
+          className="reset"
+          onClick={() => {
+            setCounter(0);
+          }}
+        >
+          Reset
+        </button>
+      </main>
     </div>
   );
 }
